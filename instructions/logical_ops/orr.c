5db319586847c8aeee461f72b7ed17ff7a476b4b
mts0629 <soratobi93@gmail.com>
2023-12-07 23:32:37 +0900

    Logical operations

---
+++ instructions/logical_ops/orr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void) {
+    uint32_t d;
+
+    __asm__(
+        "mov    r0, #5\n\t"
+        "mov    r1, #3\n\t"
+        "orr    r0, r0, r1\n\t" // r0 = r0 (0b'0101) | r1 (0b'0011) = 7 (0b'0111)
+        "mov    %[Rd], r0\n\t"
+        : [Rd] "=r" (d)
+        :
+        : "r0", "r1"
+    );
+
+    printf("r0 = 0x%08x\n", d);
+
+    return 0;
+}
