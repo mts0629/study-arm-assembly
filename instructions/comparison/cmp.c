4c7a4eada623935ca7754c24af5b2b8d8b624c80
mts0629 <soratobi93@gmail.com>
2023-12-08 23:54:01 +0900

    Comparison

---
+++ instructions/comparison/cmp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "utils/utils.h"
+
+int main(void) {
+    uint32_t cpsr1, cpsr2;
+
+    __asm__(
+        "msr    cpsr_f, #0\n\t"
+        "ldr    r0, =1\n\t"
+        "ldr    r1, =2\n\t"
+        "cmp    r0, r1\n\t" // r0 - r1 -> cpsr (negative)
+        "mrs    %[CPSR1], cpsr\n\t"
+        "ldr    r0, =10\n\t"
+        "ldr    r1, =10\n\t"
+        "cmp    r0, r1\n\t" // r0 - r1 -> cpsr (zero)
+        "mrs    %[CPSR2], cpsr\n\t"
+        : [CPSR1] "=r" (cpsr1), [CPSR2] "=r" (cpsr2)
+        :
+        : "r0", "r1"
+    );
+
+    printf("flag1: n=%u, z=%u, c=%u, v=%u\n",
+        CPSR_FLAGS_N(cpsr1), CPSR_FLAGS_Z(cpsr1), CPSR_FLAGS_C(cpsr1), CPSR_FLAGS_V(cpsr1));
+    printf("flag2: n=%u, z=%u, c=%u, v=%u\n",
+        CPSR_FLAGS_N(cpsr2), CPSR_FLAGS_Z(cpsr2), CPSR_FLAGS_C(cpsr2), CPSR_FLAGS_V(cpsr2));
+
+    return 0;
+}
