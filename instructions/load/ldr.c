8619b85b8d031f11685ec43fc423b9df32c2cabf
mts0629 <soratobi93@gmail.com>
2023-12-15 23:58:56 +0900

    Add comments

--- instructions/load/ldr.c
+++ instructions/load/ldr.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 void ldr_pre_index(void) {
-    uint32_t d[2] = { 1, 2 };
+    uint32_t d[] = { 1, 2 };
     uint32_t d0, d1;
 
     __asm__(
-        "ldr    r0, [%[Rs]]\n\t"       // r0 = *(&d) (preindex)
-        "ldr    r1, [%[Rs], #0x4]\n\t" // r1 = *(&d + 4 B) (preindex)
+        "ldr    r0, [%[Rs]]\n\t"        // r0 = *(&d) (pre-index)
+        "ldr    r1, [%[Rs], #0x4]\n\t"  // r1 = *(&d + 4 B) (= *&d[1]) (pre-index)
         "mov    %[Rd0], r0\n\t"
         "mov    %[Rd1], r1\n\t"
         : [Rd0] "=r" (d0), [Rd1] "=r" (d1)
