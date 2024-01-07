02bf9d6215cc4db6624dd1bc89e7625a65cde96c
mts0629 <soratobi93@gmail.com>
2024-01-08 00:47:20 +0900

    Call a function with multiple arguments in armeabi

--- assembly_and_c/main.c
+++ assembly_and_c/main.c
@@ -2,11 +2,14 @@
 
 int addvalues(int a, int b);
 
+int add6values(int a, int b, int c, int d, int e, int f);
+
 int main(void) {
     int a = 1, b = 2;
-    int c = addvalues(a, b);
+    printf("1 + 2 = %d\n", addvalues(a, b)); // = 3
 
-    printf("c = %d\n", c);
+    int c = 3, d = 4, e = 5, f = 6;
+    printf("(sum of 1 to 6) = %d\n", add6values(a, b, c, d, e, f)); // = 21
 
     return 0;
 }
