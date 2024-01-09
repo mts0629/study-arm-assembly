9587d9be47885fa9ccbeb44adc4e773255403cc8
mts0629 <soratobi93@gmail.com>
2024-01-09 23:50:15 +0900

    Define a static array

--- assembly_and_c/main.c
+++ assembly_and_c/main.c
@@ -4,12 +4,18 @@ int addvalues(int a, int b);
 
 int add6values(int a, int b, int c, int d, int e, int f);
 
+extern int word_data[];
+
 int main(void) {
     int a = 1, b = 2;
     printf("1 + 2 = %d\n", addvalues(a, b)); // = 3
 
     int c = 3, d = 4, e = 5, f = 6;
     printf("(sum of 1 to 6) = %d\n", add6values(a, b, c, d, e, f)); // = 21
+
+    for (int i = 0; i < 3; i++) {
+        printf("word_data[%d] = 0x%08x\n", i, word_data[i]);
+    }
 
     return 0;
 }
